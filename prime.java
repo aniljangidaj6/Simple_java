83749da66c135d283acde1451aa6c5c37240402f
Anil Jangid <31877069+aniljangidaj6@users.noreply.github.com>
2018-08-14 00:12:55 +0530

    prime.java
    Whether the number is prime or not

---
+++ prime.java
@@ -0,0 +1,21 @@
+class prime
+{
+	public static void main(String args[])
+	{
+	int num=7; int temp=0;
+		for(int i=2;i<=num-1;i++)
+		{ 
+			if(num%i==0)
+			{
+				temp=temp+1;
+			}
+			
+		}
+		if(temp>0)
+		{
+		System.out.println("prime");
+		}else
+		{System.out.println("not prime");}
+	
+	}
+}
